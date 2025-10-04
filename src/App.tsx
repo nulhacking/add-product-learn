45c35a6c840bbf5c99f00fe34c14007dee534a30
Murodillo <modernprogrammer1@gmail.com>
2025-10-04 11:37:41 +0500

    feat: last changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button, Table, type TableColumnsType } from "antd";
+import "./styles.css";
+import "@ant-design/v5-patch-for-react-19";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const columns: TableColumnsType = [
+    {
+      title: "Nomi",
+    },
+    {
+      title: "Soni",
+    },
+    {
+      title: "Turi",
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col justify-start gap-4 h-[100vh] p-4 bg-gray-300">
+      <Button type="primary" className="w-max">
+        Qo'shish
+      </Button>
+      <Table columns={columns} className="w-full" />
+    </div>
+  );
 }
 
-export default App
+export default App;
